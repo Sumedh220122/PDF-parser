78e6e51cd8662de27ff1b714515db2fe39380c03
Sumedh R <132156866+Sumedh220122@users.noreply.github.com>
2025-01-26 10:33:34 +0530

    Add files via upload

---
+++ extractor.py
@@ -0,0 +1,26 @@
+from langchain_mistralai.chat_models import ChatMistralAI
+from pydantic import BaseModel, Field
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_google_genai import ChatGoogleGenerativeAI
+from schema import SchemaReviews
+
+
+loader = PyPDFLoader("C:/Users/ravi_/Downloads/Sumedh_cloud.pdf")
+pages = loader.load_and_split()
+
+text = " ".join(list(map(lambda page: page.page_content, pages)))
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-1.5-pro",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+    api_key = 'AIzaSyCaQBZEg1ho_NqVezEMWHlKK9Uc-dXmFI4'
+)
+
+structured_llm = llm.with_structured_output(SchemaReviews)
+
+response = structured_llm.invoke(text)
+
+print(response)
---
+++ schema.py
@@ -0,0 +1,4 @@
+from pydantic import BaseModel
+
+class SchemaReviews(BaseModel):
+    content:str
